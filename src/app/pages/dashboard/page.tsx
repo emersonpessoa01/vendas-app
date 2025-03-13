34a9ffce4f9054d22d1647b64bb44b85b19bb900
emersonpessoa01 <eps011108@gmail.com>
2025-03-13 20:46:23 -0300

    feat:  Added mensagem :rocket:

--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
     return (
       <div>
-        <h1>Página Dashaboard</h1>
+        <h1>Página Dashboard</h1>
       </div>
     );
 }
---
+++ src/app/pages/mensagem/page.tsx
@@ -0,0 +1,12 @@
+// app/mensagem/page.tsx
+interface MensagemProps {
+  mensagem: number;
+}
+
+const Mensagem: React.FC<MensagemProps> = ({ mensagem }) => {
+  return <div>Mensagem: {mensagem}</div>;
+};
+
+export default function MensagemPage() {
+  return <div><Mensagem mensagem={10} /></div>;
+}
