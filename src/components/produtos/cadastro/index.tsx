23805fe9caa5fa88c9c34056641d0e7a1896848a
emersonpessoa01 <eps011108@gmail.com>
2025-03-20 21:39:15 -0300

    Upload initial :rocket:

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,45 +1,56 @@
+import React from "react";
 import { Layout } from "components";
 export const CadastroProdutos: React.FC = () => {
   return (
     <Layout titulo="Cadastro de produtos">
-      <div className="field">
-        <label htmlFor="inputSku" className="label">
-          SKU:*
-        </label>
-        <div className="control">
-          <input id="inputSku" autoFocus className="input" type="text" placeholder="Digite o SKU do produto" />
+      <div className="columns">
+        <div className="field column is-half">
+          <label htmlFor="inputSku" className="label">
+            SKU:*
+          </label>
+          <div className="control">
+            <input id="inputSku" autoFocus className="input" type="text" placeholder="Digite o SKU do produto" />
+          </div>
+        </div>
+        <div className="field column is-half">
+          <label htmlFor="inputPreco" className="label">
+            Preço:*
+          </label>
+          <div className="control">
+            <input id="inputPreco" className="input" type="text" placeholder="Digite o preço do produto" />
+          </div>
         </div>
       </div>
-      <div className="field">
-        <label htmlFor="inputPreco" className="label">
-          Preço:*
-        </label>
-        <div className="control">
-          <input id="inputPreco" className="input" type="text" placeholder="Digite o preço do produto" />
+      <div className="columns">
+        <div className="field column is-full">
+          <label htmlFor="inputNome" className="label">
+            Nome:*
+          </label>
+          <div className="control">
+            <input id="inputNome" className="input" type="text" placeholder="Digite o nome do produto" />
+          </div>
         </div>
       </div>
-      <div className="field">
-        <label htmlFor="inputNome" className="label">
-          Nome:*
-        </label>
-        <div className="control">
-          <input id="inputNome" className="input" type="text" placeholder="Digite o nome do produto" />
-        </div>
-      </div>
-      <div className="field">
-        <label htmlFor="inputDesc" className="label">
-          Descrição:*
-        </label>
-        <div className="control">
-          <textarea id="inputDesc" className="textarea" placeholder="Digite a descriação detalhada do produto" />
-        </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button type="button" className="button is-link">Salvar</button>
-        </div>
-        <div className="control">
-          <button type="button" className="button is-link is-light">Voltar</button>
+      <div className="columns">
+        <div className="field column is-full">
+          <label htmlFor="inputDesc" className="label">
+            Descrição:*
+          </label>
+          <div className="control">
+            <textarea id="inputDesc" className="textarea" placeholder="Digite a descriação detalhada do produto" />
+          </div>
+          <div className="field is-grouped py-5">
+            <div className="control">
+              <button type="button" className="button is-link">
+                Salvar
+              </button>
+            </div>
+            <div className="control">
+              <button type="button" className="button is-link is-light">
+                Voltar
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
